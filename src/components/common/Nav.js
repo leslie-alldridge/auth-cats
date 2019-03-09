f901057070832ee4625a395440c06a8881742a74
leslie.alldridge <leslie.alldridge@gmail.com>
2019-03-10 09:09:44 +1300

    sketch

--- src/components/common/Nav.js
+++ src/components/common/Nav.js
@@ -4,7 +4,7 @@ export const Nav = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <a className="navbar-brand" href="/">
-        Navbar
+        Auth Cats
       </a>
       <button
         className="navbar-toggler"
