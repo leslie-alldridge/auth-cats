63fd95593110d7ee79816e8d68add4f900e78ca3
leslie.alldridge <leslie.alldridge@gmail.com>
2019-03-10 10:03:02 +1300

    changed logic for headers

--- src/actions/cats/getAll.js
+++ src/actions/cats/getAll.js
@@ -1,19 +1,20 @@
 import axios from "axios";
-
-//stretch goal - move this into a utilities file so we don't need to write it in every single action
-
-// get the token from localstorage
 import { get } from "../../authUtilities/localstorage";
-const token = get("token");
-
-//append the token to our API requests - without this you'll get an access forbidden error
-axios.defaults.headers.common = { Authorization: `bearer ${token}` };
+//stretch goal - move this into a utilities file so we don't need to write it in every single action
 
 // This will make an API request to get all cats, while telling redux its loading and what response comes back
 export function getAllAction() {
+  // get the token from localstorage
+
+  const token = get("token");
+
+  //append the token to our API requests - without this you'll get an access forbidden error
+  axios.defaults.headers.common = { Authorization: `bearer ${token}` };
   return function(dispatch) {
     dispatch(loading());
     axios.get("/api/v1/cats/").then(response => {
+      console.log(response);
+
       if (!response.status == 200) {
         dispatch(errorMessage(response.status));
       } else {
