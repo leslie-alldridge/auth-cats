08794a4f00017f469a25100ce88c96d0dfceaae4
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-04-30 11:26:10 +1200

    comments
    comments

--- server/auth/crypto.js
+++ server/auth/crypto.js
@@ -1,5 +1,7 @@
 var bcrypt = require("bcrypt");
 
+//generate and compare functions use bcrypts built in .hash and .compare functions to hash the password and compare will unhash and compare the passwords for us
+
 function generate(password, cb) {
   bcrypt.hash(password, 12, cb);
 }
